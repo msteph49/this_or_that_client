49c0e8e601e742ea2928c1c3b3493c543d0a0c51
Michael Stephens <mdstephens26@yahoo.com>
2021-09-20 19:02:23 -0400

    rendered category title

--- src/components/category.js
+++ src/components/category.js
@@ -3,17 +3,24 @@ class Category {
 
     constructor(data) {
         this.data = data
-
-        this.contructor.all.push(this)
+        
+        // console.log(this.constructor.all.push(this))
+        this.constructor.all.push(this)
     }
 
     static getCategories = () => {
-        const categories = []
+        const categories = [{"title": "Metal or Rock n' Roll"}]
+        categories.forEach((category) => {new Category(category)})
         this.renderIndex()
     }
 
     static renderIndex = () => {
         const main = document.getElementById("app")
         main.innerHTML = `<h3>Select a Category</h3>`
+        this.all.map((category) => { main.innerHTML += category.data["title"] })
+        console.log(this.all)
+
     }
+
+
 }
