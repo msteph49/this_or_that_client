fef3413b2b7e8bbe371027191699ad23fe055127
Michael Stephens <mdstephens26@yahoo.com>
2021-09-22 16:36:00 -0400

    Add image card

--- src/components/category.js
+++ src/components/category.js
@@ -9,7 +9,7 @@ class Category {
     }
 
     static getCategories = () => {
-        const categories = [{"title": "Metal or Rock n' Roll"}]
+        const categories = [{"title": "Metal or Rock n' Roll"}, {"title": "R&B"}]
         categories.forEach((category) => {new Category(category)})
         this.renderIndex()
     }
@@ -17,7 +17,18 @@ class Category {
     static renderIndex = () => {
         const main = document.getElementById("app")
         main.innerHTML = `<h3>Select a Category</h3>`
-        this.all.map((category) => { main.innerHTML += category.data["title"] })
+        this.all.map((category) => {
+            main.innerHTML += `
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${category.data["title"]}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            `
+        })
         console.log(this.all)
 
     }
