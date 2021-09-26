1a50e59936f8c98a15aa6cf71fefd8244d5d0f20
Michael Stephens <mdstephens26@yahoo.com>
2021-09-26 17:21:27 -0400

    Attaching API service

--- src/components/category.js
+++ src/components/category.js
@@ -9,17 +9,27 @@ class Category {
     }
 
     static getCategories = () => {
-        const categories = [{"title": "Metal or Rock n' Roll","image_url": "https://picsum.photos/200/300"}, {"title": "R&B","image_url": "https://picsum.photos/200/300"}]
-        categories.forEach((category) => {new Category(category)})
-        this.renderIndex()
+        // const categories = [{"title": "Metal or Rock n' Roll","image_url": "https://picsum.photos/200/300","id": 1},
+        // {"title": "R&B","image_url": "https://picsum.photos/200/300","id": 2}]
+        api.fetchCategories().then(
+            (categories) => {
+                categories.forEach((category) => {new Category(category)})
+        
+                this.renderIndex()
+            }
+        )
+        
     }
+
+    static find = (id) => this.all.find((category) => { category.data.id == id })
+
 
     static handleClick(event){
         event.preventDefault()
 
-        console.log(event.target.closest(".card"))
+        const id = event.target.closest(".card").dataset.id
+        console.log(this.find())
     }
-    
     static renderIndex = () => {
         const main = document.getElementById("app")
         main.innerHTML = `<h3>Select a Category</h3>`
@@ -33,7 +43,7 @@ class Category {
             const col = document.createElement("div")
             col.classList.add("col")
             col.innerHTML += `
-            <div class="card">
+            <div class="card" data-id="${category.data["id"]}">
                 <img src="${category.data["image_url"]}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${category.data["title"]}</h5>
@@ -47,6 +57,8 @@ class Category {
         categoryContainer.append(row)
         console.log(this.all)
         main.append(categoryContainer)
+
+
         categoryContainer.addEventListener("click", (event) => this.handleClick(event))
     }
 
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -3,5 +3,6 @@ class ApiService {
         this.api = api
     }
 
+    fetchCategories = () => fetch(`${this.api}/categories`).then((response) => response.json())
     
 }
